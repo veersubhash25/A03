3941677e617badf68f777c96556d8b2fd3f8ead4
Denise Case <dcase@nwmissouri.edu>
2018-02-11 13:51:40 -0600

    update spring 2018

--- README.md
+++ README.md
@@ -2,28 +2,61 @@
 
 A simple chat demo using node.js, express, and socket.io
 
-## How to use
+https://bitbucket.org/professorcase/w06
 
-Open a command window in your c:\44563\w06 folder.
+## Requirements
 
-Run npm install to install all the dependencies in the package.json file.
+- Install Git version control system
+- Install Node.js, an open-source, cross-platform JavaScript run-time environment for executing JavaScript code on the server. 
+- Install Visual Studio Code for editing. 
 
-Run node server.js to start the server.  (Hit CTRL-C to stop.)
+## Get the Code
 
-```
+1. Log in to BitBucket. 
+
+2. Fork this repo from this BitBucket cloud repository into your own cloud repository.
+
+3. Clone it from your cloud repository down to your local machine. 
+
+## Run it
+
+Open a command window in your w06 folder.
+
+Run **npm install** to install all the dependencies in the package.json file.
+
+Run **node server.js** or **node server** to start the server.  (Hit CTRL-C to stop.)
+
+```Bash
 > npm install
-> node server.js
+> node server
 ```
 
 Point your browser to `http://localhost:8081`.
 
-## Reference
+## References
 
-http://javabeginnerstutorial.com/javascript-2/create-simple-chat-application-using-node-js-express-js-socket-io/
+- http://javabeginnerstutorial.com/javascript-2/create-simple-chat-application-using-node-js-express-js-socket-io/
 
-http://socket.io/get-started/chat/
+- http://socket.io/get-started/chat/
 
-https://github.com/socketio/socket.io
+- https://github.com/socketio/socket.io
 
-http://socket.io/
+- http://socket.io/
 
+## Chat with Friends
+
+Open commnand window, type **ipconfig**. 
+
+```PowerShell
+ipconfig
+```
+
+Find your temporary IPv4 Address. It may change frequently for security reasons. 
+
+Have your friends open a browser to your IP and port to see if they can join your chat app. For example: 
+
+   http://192.168.1.11:8081/
+
+   Can they hit your app?
+
+   Can you hit theirs?
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-var express = require('express');  // require express
-var app = express();  // create a request handler function
-var server = require('http').createServer(app);  // use our app to create a server
-var io = require('socket.io')(server); // pass in our http app server to get a Socket.io server
-var path = require('path');
+var express = require('express')  // require express
+var app = express()  // create a request handler function
+var server = require('http').createServer(app)  // use our app to create a server
+var io = require('socket.io')(server) // pass in our http app server to get a Socket.io server
+var path = require('path')
 
 // include the static client-side files (.html, .css, .js)
-app.use(express.static(path.join(__dirname)));
- 
-// on a GET request to default page, serve up our index.html.... 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../w06/assets', 'index.html'));
-});
- 
+app.use(express.static(path.join(__dirname)))
+
+// on a GET request to default page, serve up our index.html....
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../w06/assets', 'index.html'))
+})
+
 // on a connection event, act as follows (socket interacts with client)
-io.on('connection', function(socket){ 
-  socket.on('chatMessage', function(from, msg){  // on getting a chatMessage event
-    io.emit('chatMessage', from, msg);  // emit it to all connected clients
-  });
-  socket.on('notifyUser', function(user){  // on getting a notifyUser event
-    io.emit('notifyUser', user);  // emit to all 
-  });
-}); 
- 
+io.on('connection', function (socket) {
+  socket.on('chatMessage', function (from, msg) {  // on getting a chatMessage event
+    io.emit('chatMessage', from, msg)  // emit it to all connected clients
+  })
+  socket.on('notifyUser', function (user) {  // on getting a notifyUser event
+    io.emit('notifyUser', user)  // emit to all
+  })
+})
+
 // Listen for an app request on port 8081
-server.listen(8081, function(){
-  console.log('listening on http://127.0.0.1:8081/');
-});
+server.listen(8081, function () {
+  console.log('listening on http://127.0.0.1:8081/')
+})
